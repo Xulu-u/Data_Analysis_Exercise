eefe3e1b5cbe52134cacf4223defb0e0eeb42847
Megaoski <oscar_2596@hotmail.com>
2020-12-22 21:08:12 +0100

    Working on heatmap visualitzation

---
+++ Data Analysis exercise/Assets/Delivery/CleanCubesUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CleanCubesUI : MonoBehaviour
+{
+    public HeatMap heatmap;
+    public Toggle toggle_heat;
+   
+
+    public void DeleteCubesOnDisableToggle()
+    {
+        if (toggle_heat.GetComponent<Toggle>().isOn == false)
+        {
+            heatmap.clearMap();
+        }
+    }
+}
